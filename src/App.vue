5f1ed89dba896979457edd47ad14c0894f83a4d4
Farooque <farooque@greenapplesolutions.com>
2025-07-09 12:46:56 +0530

    svg changes done.

--- src/App.vue
+++ src/App.vue
@@ -357,7 +357,7 @@ export default {
         <div class="filter-item" @click="getCategoriesList">
           <button id="category">CATEGORY</button>
           <span class="svg-drop-down-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="12px" height="12px" viewBox="0 0 24 16" fill="none"
+            <svg xmlns="http://www.w3.org/2000/svg" width="14px" height="20px" viewBox="0 0 28 16" fill="none"
               :class="{ 'rotated': showCategories }">
               <path fill-rule="evenodd" clip-rule="evenodd" stroke="#000" stroke-width="2"
                 d="M12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L6.29289 9.70711C5.90237 9.31658 5.90237 8.68342 6.29289 8.29289C6.68342 7.90237 7.31658 7.90237 7.70711 8.29289L12 12.5858L16.2929 8.29289C16.6834 7.90237 17.3166 7.90237 17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711L12.7071 14.7071Z"
@@ -390,7 +390,7 @@ export default {
             <button v-show="this.selectedCategory" @click.stop="resetFilters"
               :class="{ 'isReset': this.selectedCategory }">Reset</button>
             <span class="svg-drop-down-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" width="12px" height="12px" viewBox="0 0 24 16" fill="none"
+              <svg xmlns="http://www.w3.org/2000/svg" width="14px" height="20px" viewBox="0 0 28 16" fill="none"
                 :class="{ 'rotated': showCategories }">
                 <path fill-rule="evenodd" clip-rule="evenodd" stroke="#000" stroke-width="2"
                   d="M12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L6.29289 9.70711C5.90237 9.31658 5.90237 8.68342 6.29289 8.29289C6.68342 7.90237 7.31658 7.90237 7.70711 8.29289L12 12.5858L16.2929 8.29289C16.6834 7.90237 17.3166 7.90237 17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711L12.7071 14.7071Z"
